56e94961d8ada63d3d0d8a6e419a0126398953c0
Christopher Serr <christopher.serr@gmail.com>
2017-06-24 02:39:54 +0200

    Free SARC Buffer

--- gh-pages.sh
+++ gh-pages.sh
@@ -2,7 +2,9 @@
 
 git branch -D gh-pages
 git checkout -b gh-pages
+cp target/asmjs-unknown-emscripten/release/js*.js* sarc.js
 git add -f sarc.js
 git commit -m "gh pages"
 git push -f origin head
 git checkout -
+cp target/asmjs-unknown-emscripten/release/js*.js* sarc.js
